34eb413f11026a344257f352cb4b20700604ba70
Криммель Герман <german@krimmel.ru>
2017-03-22 23:22:43 +0300

    Внесены небольшие правки по устранению мелких избытков

--- KNT/App.xaml.cs
+++ KNT/App.xaml.cs
@@ -6,12 +6,12 @@ namespace KNT
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         protected override void OnStartup(StartupEventArgs e)
         {
             // Get Reference to the current Process
-            Process thisProc = Process.GetCurrentProcess();
+            var thisProc = Process.GetCurrentProcess();
             // Check how many total processes have the same name as the current one
             if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
             {
