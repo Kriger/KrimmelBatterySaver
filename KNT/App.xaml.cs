2ee82d13fddf1cd0d171ab2784be14d7030ea78a
German Krimmel <german@krimmel.ru>
2017-09-17 13:50:05 +0300

    123

--- KNT/App.xaml.cs
+++ KNT/App.xaml.cs
@@ -14,7 +14,7 @@ namespace KNT
             var thisProc = Process.GetCurrentProcess();
             // Check how many total processes have the same name as the current one
             if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
-            {
+            { 
                 // If ther is more than one, than it is already running.
                 MessageBox.Show(KNT.Properties.Resources.ErrorMessage, KNT.Properties.Resources.ErrorTitle, MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 Current.Shutdown();
