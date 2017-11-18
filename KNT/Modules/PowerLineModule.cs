e0bdc2f2b6404c4d93c98a9361b2dc010b0f18eb
German Krimmel <german@krimmel.ru>
2017-11-18 07:24:12 +0300

    Добавлены комментарии к коду

--- KNT/Modules/PowerLineModule.cs
+++ KNT/Modules/PowerLineModule.cs
@@ -8,6 +8,8 @@ namespace KNT.Modules
     {
         private PowerLineStatus _currentPowerLineStatus;
 
+        // Module constructor 
+        // Subscribe module on event and depending on the power status select a power plan
         public PowerLineModule(NotifyIcon notifyIcon) : base(notifyIcon)
         {
             SystemEvents.PowerModeChanged += HandleEvent;
@@ -28,6 +30,8 @@ namespace KNT.Modules
             _currentPowerLineStatus = SystemInformation.PowerStatus.PowerLineStatus;
         }
 
+        // Processing events
+        // Switching power plan and notifications to users
         private void HandleEvent(object sender, PowerModeChangedEventArgs e)
         {
             if (e.Mode != PowerModes.StatusChange) return;
@@ -57,29 +61,35 @@ namespace KNT.Modules
 
             _currentPowerLineStatus = SystemInformation.PowerStatus.PowerLineStatus;
         }
-        
+
+        // Wrapper for subscription to PowerModeChanged event
         public override void EnableModule()
         {
             SystemEvents.PowerModeChanged += HandleEvent;
         }
 
+        // Wrapper to unsubscribe from PowerModeChanged event
         public override void DisableModule()
         {
             SystemEvents.PowerModeChanged -= HandleEvent;
         }
 
+        // Makes energy saving power scheme active on
         private void EnablePowerSaving()
         {
             RunCmdCommand("powercfg /s a1841308-3541-4fab-bc81-f71556f20b4a");
         }
 
+        // Makes high performance power scheme active on
         private void DisablePowerSaving()
         {
             RunCmdCommand("powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c");
         }
 
+        // Wrapper for the command
         private static void RunCmdCommand(string command)
         {
+            //
             var cmd = new Process
             {
                 StartInfo =
