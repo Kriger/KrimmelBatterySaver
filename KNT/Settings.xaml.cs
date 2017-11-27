44252f61491e0b8b722ae080de6c878c42ec74cc
German Krimmel <german@krimmel.ru>
2017-11-28 01:33:20 +0300

    Сохранение изменения настроек энергопитания

--- KNT/Settings.xaml.cs
+++ KNT/Settings.xaml.cs
@@ -4,6 +4,10 @@ namespace KNT
 {
     public partial class Settings
     {
+        private string highPerformance = Properties.Resources.HighPerformanceGUID;
+        private string balanceMode = Properties.Resources.BalancedModeGUID;
+        private string energySaving = Properties.Resources.EnergySavingGUID;
+
         public Settings()
         {
             InitializeComponent();
@@ -13,18 +17,24 @@ namespace KNT
             cb_language.Items.Add("English");
             cb_language.SelectedIndex = 0;
 
-            cb_online.Items.Add("Высокая производительность");
-            cb_online.Items.Add("Сбалансированный режим");
-            cb_online.SelectedIndex = 0;
+            cb_online.Items.Add(Properties.Resources.HighPerformanceTitle);
+            cb_online.Items.Add(Properties.Resources.BalancedModeTitle);
+            cb_online.SelectedIndex = Properties.Settings.Default.OnlineModeGUID == new System.Guid(highPerformance) ? 0 : 1;
 
-            cb_offline.Items.Add("Экономия заряда");
-            cb_offline.Items.Add("Сбалансированный режим");
-            cb_offline.SelectedIndex = 0;
+            cb_offline.Items.Add(Properties.Resources.BalancedModeTitle);
+            cb_offline.Items.Add(Properties.Resources.EnergySavingTitle);
+            cb_offline.SelectedIndex = Properties.Settings.Default.OfflineModeGUID == new System.Guid(balanceMode) ? 0 : 1;
         }
 
         private void SaveSettings()
         {
-            
+            var onlineGuid = new System.Guid(cb_online.SelectedIndex == 0 ? highPerformance : balanceMode);
+            var offlineGuid = new System.Guid(cb_online.SelectedIndex == 0 ? energySaving : balanceMode);
+
+            Properties.Settings.Default["SelectedLanguage"] = cb_language.SelectedValue;
+            Properties.Settings.Default["OnlineModeGUID"] = onlineGuid;
+            Properties.Settings.Default["OfflineModeGUID"] = offlineGuid;
+            Properties.Settings.Default.Save();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
